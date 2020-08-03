00758105cade2c873523a4670e440c0bd5689d48
Stefan Stefan <stefan97@yahoo.com>
2020-08-03 15:16:53 +0200

    search mechanism

--- src/components/content/content.component.jsx
+++ src/components/content/content.component.jsx
@@ -6,11 +6,17 @@ function Content() {
 
     const [videos, setVideos] = useState(null);
 
+    const [trendingButton, setTrendingButton] = useState(false);
+
     useEffect( () => {
-        fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=16&regionCode=RS&key=AIzaSyC22PzHEq4j0q7OOp7ZSharUT7bPt5LuCk')
+        fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=RS&key=AIzaSyC22PzHEq4j0q7OOp7ZSharUT7bPt5LuCk')
             .then(response => response.json())
             .then(data => setVideos(data));   
     }, []);
+
+    function handleTrendingClick() {
+        setTrendingButton(true);
+    }
 
     return(
         <div className='content'>
@@ -19,7 +25,44 @@ function Content() {
             </div>
             <div className='videos-container'>
             {videos === null ? null : 
-                videos.items.map((video, idx) => <HomepageVideo key={idx} data={video}/>)
+                videos.items.map((video, idx) => {
+                    if(idx <= 7) {
+                        return <HomepageVideo key={idx} data={video}/>
+                    } else {
+                        return null;
+                    }
+                })
+            }
+            </div>
+            <hr className='content-hr'/>
+            <div className='content-title content-title-trending'>
+                <p>Trending</p>
+            </div>
+            <div className='videos-container'>
+            {videos === null ? null : 
+                videos.items.map((video, idx) => {
+                    if(idx > 7 && (trendingButton ? (idx < 20) : (idx < 12))) {
+                        return <HomepageVideo key={idx} data={video}/>
+                    } else {
+                        return null;
+                    }
+                })
+            }
+            </div>
+            {trendingButton ? (
+                <hr className='content-hr trending-hr'/>
+            ) : (
+                <div className='trending-arrow' onClick={handleTrendingClick}><i class="material-icons">keyboard_arrow_down</i></div>
+            )}
+            <div className='videos-container'>
+            {videos === null ? null : 
+                videos.items.map((video, idx) => {
+                    if(idx >= 20) {
+                        return <HomepageVideo key={idx} data={video}/>
+                    } else {
+                        return null;
+                    }
+                })
             }
             </div>
         </div>
